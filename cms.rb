fcb35c1476f591a860775f5e4cd71d1e62b50609
William Reilly <William.Reilly2010@gmail.com>
2016-03-20 14:20:45 -0400

    My take on viewing text files. I did have to use the solution because I could not find a way to show the plain text without having to put in markup language around each line.

--- cms.rb
+++ cms.rb
@@ -6,3 +6,10 @@ get '/' do
   @files = Dir.glob("data/*.txt").map {|path| File.basename(path)}
   erb :index
 end
+
+get "/*.txt" do
+  file = params['splat'].first + ".txt"
+  
+  headers['Content-Type'] = 'text/plain'
+  File.read("data/#{file}")
+end
